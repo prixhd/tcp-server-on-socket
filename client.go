e4cdc92869ebaa1ff9d524d090e1a81ecf4bd199
a1234 <manap.aminov@gmail.com>
2026-09-12 23:12:18 +0300

    create a client

---
+++ client.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
+
+func main() {
+
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Error connecting to server:", err)
+		return
+	}
+
+	defer conn.Close()
+
+	io.Copy(os.Stdout, conn)
+	fmt.Println("\nConnection closed.")
+}
